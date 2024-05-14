9348ededa034f7679654cd0357ddb30a69e43771
babermkhan <65040303+babermkhan@users.noreply.github.com>
2024-05-14 21:05:12 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const myId = 2555;
+let myEmail = "Helloworld@yahoo.com";
+var accountPassword = 2555;
+
+console.table([myId, myEmail, accountPassword]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+// alert("Hello") we are using nodejs, not browser
+
+let name = "Baber";
+let age = 45;
+let isLoggedIn = false;
+let state;
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
